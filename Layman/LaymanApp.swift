394272a209a86ded454737563cfb47b6ba91906f
Pranjal Shinde <shinde.pc1707@gmail.com>
2026-03-31 18:52:08 +0530

    Initial Commit

---
+++ Layman/LaymanApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaymanApp.swift
+//  Layman
+//
+//  Created by Pranjal Shinde on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct LaymanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
